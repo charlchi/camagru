cf39ace98acc75d8b5de4e426278a335a6fb4baa
Charl MOLLER <cmoller@c1r1s4.wethinkcode.co.za>
2018-11-09 11:59:35 +0200

    hi

--- reaction.php
+++ reaction.php
@@ -8,23 +8,31 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 	$post = $_POST;
 	$db = db_open();
 	try {
-		$post_id = intval($post['id']);
+		$pid = intval($post['id']);
+		if (intval($post['type']) == 1)
+		{
+			$uid = get_id($_COOKIE['username']);
+			$sth = $db->prepare("SELECT * FROM reactions WHERE user_id = $uid AND post_id = $pid");
+			$sth->execute();
+			$user = $sth->fetch();
+			die("Comment already liked.");
+		}
 		$stm = $db->prepare("INSERT INTO reactions ".
 			"(user_id, post_id, type, data, date)".
 			"VALUES (?, ?, ?, ?, ?)");
 		$stm->execute(array(
 			get_id($_COOKIE['username']),
-			$post_id,
+			$pid,
 			intval($post['type']),
 			$post['msg'],
 			time()
 		));
 		if (intval($post['type']) == 0) {
-			$sth = $db->prepare("SELECT user_id FROM posts WHERE ID = $post_id");
+			$sth = $db->prepare("SELECT user_id FROM posts WHERE ID = $pid");
 			$sth->execute();
 			$user_id = $sth->fetch();
 			$user_id = $user_id['user_id'];
-			$sth = $db->prepare("SELECT * FROM users WHERE ID = $user_id");
+			$sth = $db->prepare("SELECT * FROM users WHERE ID = $uid");
 			$sth->execute();
 			$user = $sth->fetch();
 			if ($user['sendmail'] == 1) {
